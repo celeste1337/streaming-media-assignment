4d4ed8ca4448f81a14ad2480accedbb71310fb7a
Celeste Yost <17mcyost@gmail.com>
2019-09-10 16:47:46 -0400

    server.js set up :)

---
+++ src/server.js
@@ -0,0 +1,15 @@
+// handles all the server stuff!!
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+const mediaHandler = require('./mediaResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// start server, listen for traffic
+const onRequest = (request, response) => {
+  console.log(request.url);
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
